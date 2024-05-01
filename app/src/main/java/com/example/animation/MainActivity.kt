cbe95c93e0b51d57b92c4e0454bdcc2e0005b45e
yuxian1121 <yuxian1121@github.com>
2024-05-01 16:43:56 +0800

    make it better

--- app/src/main/java/com/example/animation/MainActivity.kt
+++ app/src/main/java/com/example/animation/MainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -27,7 +32,17 @@ import androidx.compose.foundation.layout.Column
 
 import androidx.compose.material3.Button
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -54,10 +69,37 @@ fun Animation(){
     var expanded by remember { mutableStateOf(true) }
     var fly by remember{mutableStateOf(false) }
 
-    Column {
+    //角度動畫
+    val buttonAngle by animateFloatAsState(
+        if (appear) 360f else 0f,
+        animationSpec = tween(durationMillis = 2500)
+    )
+    //顏色動畫
+    val backgroundColor by animateColorAsState(
+        if (appear) Color.Transparent else Color.White,
+        animationSpec = tween(2000, 500)
+    )
+    //大小動畫
+    val rocketSize by animateDpAsState(
+        if (fly) 75.dp else 150.dp,
+        animationSpec = tween(2000)
+    )
+    //位移動畫
+    val rocketOffset by animateOffsetAsState(
+        if (fly) Offset(200f, -50f) else Offset(200f, 400f),
+        animationSpec = tween(2000)
+    )
+
+
+
+
+
+    Column (Modifier.background(backgroundColor)){
         Button(onClick = {
             appear = !appear
-        })
+        },
+            modifier = Modifier.rotate(buttonAngle)
+        )
         {
             if (appear) Text(text = "星空背景圖消失")
             else Text(text = "星空背景圖出現")
@@ -82,9 +124,32 @@ fun Animation(){
         ) {
             Image(
                 painter = painterResource(id = R.drawable.sky),
-                contentDescription = "星空背景圖"
+                contentDescription = "星空背景圖",
+                modifier = Modifier
+                    .animateContentSize()
+                    .fillMaxWidth()
+                    .height(if (expanded) 600.dp else 400.dp)
+                    .clickable(
+                    ) {
+                        expanded = !expanded
+                    }
+
             )
+            Image(
+                painter = painterResource(id = R.drawable.rocket),
+                contentDescription = "火箭",
+                modifier = Modifier
+                    .size(rocketSize)
+                    .offset(rocketOffset.x.dp, rocketOffset.y.dp)
+                    .clickable(
+                    ) {
+                        fly = !fly
+                    }
+            )
+
+
         }
+
 
     }
 
